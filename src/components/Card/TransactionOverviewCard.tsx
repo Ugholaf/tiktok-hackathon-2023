c3cd276da8a4fb1fb7429dec9ac853383ddbbb57
Wu Changjun <wuchangjun98@hotmail.com>
2023-09-02 18:05:00 +0800

    add transactions

--- src/components/Card/TransactionOverviewCard.tsx
+++ src/components/Card/TransactionOverviewCard.tsx
@@ -1,11 +1,11 @@
 const TransactionOverviewCard = () => {
   return (
-    <div className="flex flex-col rounded-xl py-6 px-11 my-6 self-stretch items-center  bg-white w-3/6">
+    <div className="flex flex-col rounded-xl py-6 px-11 my-6 self-stretch items-center bg-white w-3/6">
       <p className="text-2xl font-bold border-b-2 border-red-600 mb-6">
         MTD Transaction Overview
       </p>
-      <div className="flex flex-col items-start self-stretch w-full">
-        <div className="flex flex-row space-between items-center self-stretch px-11 py-3 bg-gray-200 w-full rounded-t-lg">
+      <div className="flex flex-col self-stretch w-full items-end">
+        <div className="flex flex-row justify-between items-center self-stretch px-11 py-3 bg-gray-200 w-full rounded-t-lg">
           <div className="flex flex-col py-1 justify-center items-center">
             <div className="flex flex-row">
               <p className="text-2xl font-bold">Payments In</p>
@@ -21,9 +21,11 @@ const TransactionOverviewCard = () => {
             <p className="text-base ">$100.00 SGD</p>
           </div>
         </div>
-        <div className="flex flex-col items-start px-3 py-3 bg-gray-100 w-full rounded-b-lg">
-          net spending
+        <div className="flex flex-col justify-center items-center px-3 py-3 bg-gray-100 w-full rounded-b-lg">
+          <p className="text-2xl font-bold">Net Spending</p>
+          <p className="text-base ">$125.00 SGD</p>
         </div>
+        <button className=" text-xs border-b border-black ">View More</button>
       </div>
     </div>
   );
--- src/pages/PersonalPage.tsx
+++ src/pages/PersonalPage.tsx
@@ -1,14 +1,20 @@
 import TransactionOverviewCard from "../components/Card/TransactionOverviewCard";
 import WalletBalanceCard from "../components/Card/WalletBalanceCard";
 import Layout from "../components/Layout/Layout";
+import RecentTransactionsCard from "../components/Card/RecentTransactionsCard";
 
 const PersonalPage = () => {
   return (
     <Layout>
       <div className="flex flex-col items-center gap-9 w-full px-24 py-9">
-        <div className="flex flex-col items-center gap-8">
+        <div className="flex flex-col items-center">
           <div className="flex justify-center items-center gap-3 self-stretch">
             <WalletBalanceCard />
+            <TransactionOverviewCard />
+          </div>
+          <div className="flex justify-center items-center gap-3 self-stretch">
+            <RecentTransactionsCard />
+            {/* replace this with contact card */}
             <TransactionOverviewCard />
           </div>
         </div>
