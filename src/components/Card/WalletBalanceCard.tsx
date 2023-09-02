b8051d76ce4de3754275e7910c763e78225ca706
Marcus Hooi <marcus.hooi@gmail.com>
2023-09-02 23:40:10 +0800

    fix autosave

--- src/components/Card/WalletBalanceCard.tsx
+++ src/components/Card/WalletBalanceCard.tsx
@@ -7,8 +7,7 @@ const WalletBalanceCard = () => {
   const [openPaypal, setOpenPaypal] = useState(false);
 
   return (
-<<<<<<< HEAD
-    <div className="flex flex-col rounded-xl py-6 px-11 my-6 self-stretch items-center w-full bg-white w-3/6">
+    <div className="flex flex-col rounded-xl py-6 px-11 my-6 self-stretch items-center w-full bg-white">
       <p className="text-2xl font-bold border-b-2 border-red-600 mb-6">
         Wallet Balance
       </p>
@@ -20,7 +19,7 @@ const WalletBalanceCard = () => {
         <div className="flex flex-col items-start px-3 py-3 bg-gray-100 w-full rounded-b-lg">
           <p className="text-base mb-2">Perform Transactions</p>
           <div className="flex flex-wrap justify-between items-center gap-3 self-stretch">
-            <button className="flex flex-row gap-1 bg-white py-2 px-3 justify-between shadow-md rounded-md">
+            <button className="flex flex-row gap-1 bg-white py-2 px-3 justify-between shadow-md rounded-md" onClick={() => setOpenCashIn(true)}>
               <img src="/src/assets/buttons/cashIn.svg" alt="cash in" />
               Cash In
             </button>
@@ -43,48 +42,6 @@ const WalletBalanceCard = () => {
               <img src="/src/assets/buttons/qrcode.svg" alt="qrcode" />
               My QR
             </button>
-=======
-    <>
-      <div className="flex flex-col rounded-xl py-6 px-11 my-6 self-stretch items-center  bg-white w-3/6">
-        <p className="text-2xl font-bold border-b-2 border-red-600 mb-6">
-          Wallet Balance
-        </p>
-        <div className="flex flex-col items-start self-stretch w-full">
-          <div className="flex flex-col items-start px-3 py-1 bg-gray-200 w-full rounded-t-lg">
-            <p className="text-3xl">$100.00 SGD</p>
-            <p className="text-base text-gray-500">Available</p>
-          </div>
-          <div className="flex flex-col items-start px-3 py-3 bg-gray-100 w-full rounded-b-lg">
-            <p className="text-base mb-2">Perform Transactions</p>
-            <div className="flex flex-wrap justify-between items-center gap-3 self-stretch">
-              <button
-                className="flex flex-row gap-1 bg-white py-2 px-3 justify-between shadow-md rounded-md"
-                onClick={() => setOpenCashIn(true)}
-              >
-                <img src="/src/assets/buttons/cashIn.svg" alt="cash in" />
-                Cash In
-              </button>
-              <button className="flex flex-row gap-1 bg-white py-2 px-3 justify-between shadow-md rounded-md">
-                <img src="/src/assets/buttons/cashOut.svg" alt="cash out" />
-                Cash Out
-              </button>
-              <button className="flex flex-row gap-1 bg-white py-2 px-3 justify-between shadow-md rounded-md">
-                <img
-                  src="/src/assets/buttons/p2pTransfer.svg"
-                  alt="p2p transfer"
-                />
-                P2P Transfer
-              </button>
-              <button className="flex flex-row gap-1 bg-white py-2 px-3 justify-between shadow-md rounded-md">
-                <img src="/src/assets/buttons/scan.svg" alt="scan" />
-                Scan
-              </button>
-              <button className="flex flex-row gap-1 bg-white py-2 px-3 justify-between shadow-md rounded-md">
-                <img src="/src/assets/buttons/qrcode.svg" alt="qrcode" />
-                My QR
-              </button>
-            </div>
->>>>>>> main
           </div>
         </div>
       </div>
@@ -94,7 +51,7 @@ const WalletBalanceCard = () => {
         setOpenPaypal={setOpenPaypal}
       />
       <PaypalModal open={openPaypal} setOpen={setOpenPaypal} />
-    </>
+    </div>
   );
 };
 
