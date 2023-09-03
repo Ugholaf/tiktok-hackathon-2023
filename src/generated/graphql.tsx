570002b659c88fd05c1fb9aa4a35efb69df8c189
Wu Changjun <56215606+Ugholaf@users.noreply.github.com>
2023-09-03 17:40:06 +0800

    add business registration (#16)

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -188,6 +188,26 @@ export type LoginMutationVariables = Exact<{
 
 export type LoginMutation = { __typename?: 'Mutation', login: { __typename?: 'AuthPayload', accessToken: string } };
 
+export type RegisterBusinessMutationVariables = Exact<{
+  username: Scalars['String']['input'];
+  email: Scalars['String']['input'];
+  password: Scalars['String']['input'];
+  firstName: Scalars['String']['input'];
+  lastName: Scalars['String']['input'];
+  dateOfBirth: Scalars['String']['input'];
+  country: Scalars['String']['input'];
+  postcode: Scalars['String']['input'];
+  occupation: Scalars['String']['input'];
+  businessName: Scalars['String']['input'];
+  uen: Scalars['String']['input'];
+  businessCountry: Scalars['String']['input'];
+  businessPostcode: Scalars['String']['input'];
+  businessAddress: Scalars['String']['input'];
+}>;
+
+
+export type RegisterBusinessMutation = { __typename?: 'Mutation', registerBusiness: { __typename?: 'AuthPayload', accessToken: string } };
+
 export type RegisterIndividualMutationVariables = Exact<{
   username: Scalars['String']['input'];
   email: Scalars['String']['input'];
@@ -243,6 +263,67 @@ export function useLoginMutation(baseOptions?: Apollo.MutationHookOptions<LoginM
 export type LoginMutationHookResult = ReturnType<typeof useLoginMutation>;
 export type LoginMutationResult = Apollo.MutationResult<LoginMutation>;
 export type LoginMutationOptions = Apollo.BaseMutationOptions<LoginMutation, LoginMutationVariables>;
+export const RegisterBusinessDocument = gql`
+    mutation RegisterBusiness($username: String!, $email: String!, $password: String!, $firstName: String!, $lastName: String!, $dateOfBirth: String!, $country: String!, $postcode: String!, $occupation: String!, $businessName: String!, $uen: String!, $businessCountry: String!, $businessPostcode: String!, $businessAddress: String!) {
+  registerBusiness(
+    username: $username
+    email: $email
+    password: $password
+    firstName: $firstName
+    lastName: $lastName
+    dateOfBirth: $dateOfBirth
+    country: $country
+    postcode: $postcode
+    occupation: $occupation
+    businessName: $businessName
+    uen: $uen
+    businessCountry: $businessCountry
+    businessPostcode: $businessPostcode
+    businessAddress: $businessAddress
+  ) {
+    accessToken
+  }
+}
+    `;
+export type RegisterBusinessMutationFn = Apollo.MutationFunction<RegisterBusinessMutation, RegisterBusinessMutationVariables>;
+
+/**
+ * __useRegisterBusinessMutation__
+ *
+ * To run a mutation, you first call `useRegisterBusinessMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useRegisterBusinessMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [registerBusinessMutation, { data, loading, error }] = useRegisterBusinessMutation({
+ *   variables: {
+ *      username: // value for 'username'
+ *      email: // value for 'email'
+ *      password: // value for 'password'
+ *      firstName: // value for 'firstName'
+ *      lastName: // value for 'lastName'
+ *      dateOfBirth: // value for 'dateOfBirth'
+ *      country: // value for 'country'
+ *      postcode: // value for 'postcode'
+ *      occupation: // value for 'occupation'
+ *      businessName: // value for 'businessName'
+ *      uen: // value for 'uen'
+ *      businessCountry: // value for 'businessCountry'
+ *      businessPostcode: // value for 'businessPostcode'
+ *      businessAddress: // value for 'businessAddress'
+ *   },
+ * });
+ */
+export function useRegisterBusinessMutation(baseOptions?: Apollo.MutationHookOptions<RegisterBusinessMutation, RegisterBusinessMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<RegisterBusinessMutation, RegisterBusinessMutationVariables>(RegisterBusinessDocument, options);
+      }
+export type RegisterBusinessMutationHookResult = ReturnType<typeof useRegisterBusinessMutation>;
+export type RegisterBusinessMutationResult = Apollo.MutationResult<RegisterBusinessMutation>;
+export type RegisterBusinessMutationOptions = Apollo.BaseMutationOptions<RegisterBusinessMutation, RegisterBusinessMutationVariables>;
 export const RegisterIndividualDocument = gql`
     mutation RegisterIndividual($username: String!, $email: String!, $password: String!, $firstName: String!, $lastName: String!, $dateOfBirth: String!, $country: String!, $postcode: String!, $occupation: String!) {
   registerIndividual(
