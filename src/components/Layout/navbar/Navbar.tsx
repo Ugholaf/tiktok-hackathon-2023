3ee26d5745bfda5f262fd566761f23055f1fc750
Marcus Hooi <marcus.hooi@gmail.com>
2023-09-03 11:34:00 +0800

    nav bar fix

--- src/components/Layout/navbar/Navbar.tsx
+++ src/components/Layout/navbar/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   const handleTabChange = (tab: string) => {
     setActiveTab(tab);
-    setShowMenu(false);
+    setShowMenu(true);  {/*Set effect to auto close menu or not or not*/}
   };
 
   const toggleMenu = () => {
@@ -26,10 +26,7 @@ const Navbar = () => {
 
           <div className="md:hidden block flex items-center text-4xl" onClick={toggleMenu}>&#9776;</div>  {/* Hamburger Menu */}
 
-
-
-
-          <ul className="hidden md:flex flex-row gap-3 items-center ">
+          <ul className="hidden md:flex flex-row gap-3 items-center "> {/* Desktop Menu tabs */}
             {tabs.map((tab) => (
               <li
                 className={`cursor-pointer font-bold ${
@@ -44,7 +41,7 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
-          <div className= "hidden md:flex flex-row gap-3 items-center">
+          <div className= "hidden md:flex flex-row gap-3 items-center"> {/* Desktop personal and name/logout */}
             <button className="bg-white text-red-600 py-2 px-4">
               Personal
             </button>
@@ -58,7 +55,7 @@ const Navbar = () => {
 
       {/* Mobile Menu Put here to that it is below the Nav bar*/}
         {showMenu && (
-          <ul className="md:hidden flex flex-col gap-3 items-center">
+          <ul className="md:hidden flex flex-col gap-3 py-3 items-center">
             {tabs.map((tab) => (
               <li
                 className={`cursor-pointer font-bold ${
@@ -70,6 +67,14 @@ const Navbar = () => {
                 {tab}
               </li>
             ))}
+            <div className="border-b-2 border-gray-400 w-full"></div> {/* Line to separate tabs and personal/logout */}
+            <div className="flex flex-row gap-3 items-center justify-between self-stretch flex-1"> 
+              <div className = "flex flex-row items-center gap-2"> 
+                <img src="/src/assets/recentcontact/person.svg" alt="person" className="h-4 w-4 stroke-red-500"/>
+                <button className="bg-white text-red-600">Personal Account</button>
+              </div>
+              <button className="bg-white">Logout</button>
+            </div>
           </ul>
         )}
     </div>
